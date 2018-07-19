fdb2e06f8c151001c81a4c9b423f3656092494c6
Guest User <Guest@Epicodus-3E.local>
2018-07-19 16:18:30 -0700

    fix database

--- database_backup.sql.old.sql
+++ database_backup.sql.old.sql
@@ -2,33 +2,32 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.6.3
--- Dumped by pg_dump version 9.6.3
+-- Dumped from database version 10.4 (Ubuntu 10.4-0ubuntu0.18.04)
+-- Dumped by pg_dump version 10.4 (Ubuntu 10.4-0ubuntu0.18.04)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
-
-SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
 
@@ -38,20 +37,20 @@ SET default_with_oids = false;
 -- Name: projects; Type: TABLE; Schema: public; Owner: Guest
 --
 
-CREATE TABLE projects (
+CREATE TABLE public.projects (
     id integer NOT NULL,
-    title character varying,
-    volunteer_id integer
+    title character varying
 );
 
 
-ALTER TABLE projects OWNER TO "Guest";
+ALTER TABLE public.projects OWNER TO Guest;
 
 --
 -- Name: projects_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
 --
 
-CREATE SEQUENCE projects_id_seq
+CREATE SEQUENCE public.projects_id_seq
+    AS integer
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -59,33 +58,34 @@ CREATE SEQUENCE projects_id_seq
     CACHE 1;
 
 
-ALTER TABLE projects_id_seq OWNER TO "Guest";
+ALTER TABLE public.projects_id_seq OWNER TO Guest;
 
 --
 -- Name: projects_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
 --
 
-ALTER SEQUENCE projects_id_seq OWNED BY projects.id;
+ALTER SEQUENCE public.projects_id_seq OWNED BY public.projects.id;
 
 
 --
 -- Name: volunteers; Type: TABLE; Schema: public; Owner: Guest
 --
 
-CREATE TABLE volunteers (
+CREATE TABLE public.volunteers (
     id integer NOT NULL,
     name character varying,
     project_id integer
 );
 
 
-ALTER TABLE volunteers OWNER TO "Guest";
+ALTER TABLE public.volunteers OWNER TO Guest;
 
 --
 -- Name: volunteers_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
 --
 
-CREATE SEQUENCE volunteers_id_seq
+CREATE SEQUENCE public.volunteers_id_seq
+    AS integer
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -93,38 +93,50 @@ CREATE SEQUENCE volunteers_id_seq
     CACHE 1;
 
 
-ALTER TABLE volunteers_id_seq OWNER TO "Guest";
+ALTER TABLE public.volunteers_id_seq OWNER TO Guest;
 
 --
 -- Name: volunteers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
 --
 
-ALTER SEQUENCE volunteers_id_seq OWNED BY volunteers.id;
+ALTER SEQUENCE public.volunteers_id_seq OWNED BY public.volunteers.id;
 
 
 --
 -- Name: projects id; Type: DEFAULT; Schema: public; Owner: Guest
 --
 
-ALTER TABLE ONLY projects ALTER COLUMN id SET DEFAULT nextval('projects_id_seq'::regclass);
+ALTER TABLE ONLY public.projects ALTER COLUMN id SET DEFAULT nextval('public.projects_id_seq'::regclass);
 
 
 --
 -- Name: volunteers id; Type: DEFAULT; Schema: public; Owner: Guest
 --
 
-ALTER TABLE ONLY volunteers ALTER COLUMN id SET DEFAULT nextval('volunteers_id_seq'::regclass);
+ALTER TABLE ONLY public.volunteers ALTER COLUMN id SET DEFAULT nextval('public.volunteers_id_seq'::regclass);
 
 
 --
 -- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: Guest
 --
 
-COPY projects (id, title, volunteer_id) FROM stdin;
-1	eat pie	\N
-2	eat cake	\N
-3	cake decorating	\N
-4	Ride bike in hot sun	\N
+COPY public.projects (id, title) FROM stdin;
+5	paint barn
+1	wash boat
+3	mow attic
+2	eat cake
+4	vacuum fish tank
+\.
+
+
+--
+-- Data for Name: volunteers; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.volunteers (id, name, project_id) FROM stdin;
+3	bob	4
+1	janet	1
+2	Jessica Smith	2
 \.
 
 
@@ -132,32 +144,21 @@ COPY projects (id, title, volunteer_id) FROM stdin;
 -- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
 --
 
-SELECT pg_catalog.setval('projects_id_seq', 4, true);
-
-
---
--- Data for Name: volunteers; Type: TABLE DATA; Schema: public; Owner: Guest
---
-
-COPY volunteers (id, name, project_id) FROM stdin;
-1	bob	1
-2	Rita	3
-3	Reese	4
-\.
+SELECT pg_catalog.setval('public.projects_id_seq', 5, true);
 
 
 --
 -- Name: volunteers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
 --
 
-SELECT pg_catalog.setval('volunteers_id_seq', 3, true);
+SELECT pg_catalog.setval('public.volunteers_id_seq', 3, true);
 
 
 --
 -- Name: projects projects_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
 --
 
-ALTER TABLE ONLY projects
+ALTER TABLE ONLY public.projects
     ADD CONSTRAINT projects_pkey PRIMARY KEY (id);
 
 
@@ -165,11 +166,10 @@ ALTER TABLE ONLY projects
 -- Name: volunteers volunteers_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
 --
 
-ALTER TABLE ONLY volunteers
+ALTER TABLE ONLY public.volunteers
     ADD CONSTRAINT volunteers_pkey PRIMARY KEY (id);
 
 
 --
 -- PostgreSQL database dump complete
 --
-
